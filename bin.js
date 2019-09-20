60eec62b7576ba95bf2cfa3b44adab0be95bedaa
Damien Duhamel <d.damien@live.fr>
2019-09-20 18:30:44 +0200

    Improve code

--- bin.js
+++ bin.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log( 'ok' );
+const serve = require('./index.js');
+
+let port = 3000;
+let http2 = false;
+let folder = process.cwd();
+
+process.argv.forEach(argv => {
+  if (argv === '--http2') http2 = true;
+  if (argv.startsWith('--path=')) folder = argv.split('=')[1];
+  if (argv.startsWith('--port=')) port = argv.split('=')[1];
+});
+
+serve(port, folder, http2);
